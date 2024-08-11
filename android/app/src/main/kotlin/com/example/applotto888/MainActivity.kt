687e15ed378e8337174f96da31bf7caea8cced8d
jomchot <jomchot.j@gmail.com>
2024-08-11 17:11:09 +0700

    addPages

---
+++ android/app/src/main/kotlin/com/example/applotto888/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.applotto888
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:applotto888/pages/user/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+    
+      home: LoginPage(),
+    );
+  }
+}
+
